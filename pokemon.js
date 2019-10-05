b8d26d8574a622b175eac603348bc15c2039f5b9
Yossaphat Karnprajam <sunsunza2009@hotmail.com>
2019-10-05 16:29:57 +0700

    Add async mongodb

--- pokemon.js
+++ pokemon.js
@@ -1,17 +1,37 @@
+const mongoCilent = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
+
 var pokeList = []
+
+const DB_URL = "mongodb+srv://admin:admin@cluster0-vydnj.gcp.mongodb.net/admin?retryWrites=true&w=majority"
+const DB_Name = "example"
+const option = {useNewUrlParser: true,useUnifiedTopology: true}
 
 class Pokemon{
 	constructor(name, type){
-		this.id = null
 		this.name = name
 		this.type = type
 		this.type2 = null
 	}
 }
 
+var collection, database, client
+var connect = async function(){
+	if(client != undefined)
+		return
+	client = await mongoCilent.connect(DB_URL, option).catch((err) => console.error(err))
+	database = client.db(DB_Name)
+}
+var Collection = async function(){
+	await connect()
+	if(collection == undefined)
+		collection = database.collection('pokemons')
+	return collection
+}
+
 var mock = function(){
-	let p = createPokemon("Eevee", "Normal")
-	pokeList.push(p)
+	//save("Eevee", "Normal")
+	//save("Mew", "Psychic")
 }
 
 var genID = function(num){
@@ -21,35 +41,91 @@ var genID = function(num){
 
 var createPokemon = function(name, type){
 	let p = new Pokemon(name, type)
-	p.id = genID(pokeList.length)
+	//p.id = genID(pokeList.length)
 	return p
 }
 
-var checkID = function(id){
-	return pokeList[id-1] !== undefined && pokeList[id-1] !== null
+var checkID = async function(id){
+	if(id.length < 24)
+		return false
+	let collection = await Collection()
+	try{
+		var result = await collection.find({_id:ObjectID(id)}).count()
+		console.log(result)
+		return result > 0
+	}catch(err){
+		console.error(err)
+		return false
+	}finally{
+		client.close()
+	}
 }
 
-var save = function(name, type){
-	let p = new Pokemon(name, type)
-	p.id = genID(pokeList.length)
-	pokeList.push(p)
-	return true
+var save = async function(name, type){
+	let p = createPokemon(name, type)
+	let collection = await Collection()
+	try{
+		var result = await collection.insertOne(p)
+		return true
+	}catch(err){
+		console.error(err)
+		return false
+	}finally{
+		client.close()
+	}
 }
 
-var get = function(id){
-	return pokeList[id]
+var getAll = async function(){
+	let collection = await Collection()
+	try{
+		var result = await collection.find({}).toArray()
+		return result
+	}catch(err){
+		console.error(err)
+		return false
+	}finally{
+		client.close()
+	}
 }
 
-var update = function(poke){
-	let p = new Pokemon(poke.name, poke.type)
-	p.id = poke.id
-	p.type2 = poke.type2
-	pokeList[poke.id - 1] = p
+var get = async function(id){
+	let collection = await Collection()
+	try{
+		var result = await collection.findOne({_id:ObjectID(id)})
+		return result
+	}catch(err){
+		console.error(err)
+		return false
+	}finally{
+		client.close()
+	}
+}
+
+var update = async function(id,poke){
+	let p = {}
+	if(poke.name != undefined)
+		p.name = poke.name
+	if(poke.type != undefined)
+		p.type = poke.type
+	if(poke.type2 != undefined)
+		p.type2 = poke.type2
+	let collection = await Collection()
+	try{
+		console.log(p)
+		var result = await collection.updateOne({_id:ObjectID(id)},{ $set: p})
+		return result
+	}catch(err){
+		console.error(err)
+		return false
+	}finally{
+		client.close()
+	}
 }
 
 var deletePoke = function(id){
 	delete pokemon.pokeList[id]
 }
+
 module.exports.checkID = checkID
 module.exports.save = save
 module.exports.pokeList = pokeList
@@ -57,3 +133,4 @@ module.exports.mock = mock
 module.exports.get = get
 module.exports.update = update
 module.exports.deletePoke = deletePoke
+module.exports.getAll = getAll
--- router.js
+++ router.js
@@ -8,48 +8,80 @@ var checkParem = function(param){
 	return param === '' || param === null || param === undefined
 }
 
-router.get('/pokemons', function (req, res) {
-	res.send(pokemon.pokeList)
+router.get('/pokemons', (req, res) =>  {
+	pokemon.getAll().then((result)=>{
+		res.send(result)
+	}).catch((err)=>{
+		console.log(err)
+		res.status(500).send({error:"Get Pokemon Unsuccessful"})
+	})
 })
 
-router.post('/pokemons', function (req, res) {
+router.post('/pokemons', (req, res) => {
 	if(checkParem(req.body.name) || checkParem(req.body.type)){
 		res.status(400).send({error:"Insufficient parameters: name and type are required"})		
 		return
 	}
-	let success = pokemon.save(req.body.name, req.body.type)
-	if(success)
-		res.status(201).send({name: req.body.name,type:req.body.type})
+	pokemon.save(req.body.name, req.body.type).then((result)=>{
+		res.sendStatus(201)
+	}).catch((err)=>{
+		res.status(500).send({error:"Insert Pokemon Unsuccessful"})
+	})
+
 })
 
-router.get('/pokemon/:id', function (req, res) {
+router.get('/pokemon/:id', (req, res) => {
 	if(checkParem(req.params.id)){
 		res.status(400).send({error:"Insufficient parameters: id is required"})		
 		return
 	}
-	if(!pokemon.checkID(req.params.id)){
-		res.status(400).send({error:"Cannot get pokemon: Pokemon not found"})		
-		return
-	}
-	res.send(pokemon.get(req.params.id-1))
+	pokemon.checkID(req.params.id).then((result)=>{
+		
+	}).catch((err)=>{
+		res.status(400).send({error:"Cannot get pokemon: Pokemon not found"})
+	})
+	pokemon.get(req.params.id).then((result)=>{
+		res.send(result)
+	}).catch((err)=>{
+		res.status(500).send({error:"Get Pokemon Unsuccessful"})
+	})
 })
 
-router.put('/pokemon/:id', function (req, res) {
+router.put('/pokemon/:id', (req, res) => {
 	if(checkParem(req.body.type2) || checkParem(req.params.id)){
 		res.status(400).send({error:"Insufficient parameters: id and type2 are required"})		
 		return
 	}
-	if(!pokemon.checkID(req.params.id)){
-		res.status(400).send({error:"Cannot update pokemon: Pokemon not found"})		
+	pokemon.checkID(req.params.id).then((result)=>{
+		if(result == false)
+		{
+			res.status(400).send({error:"Cannot get pokemon: Pokemon not found"})	
+			return
+		}
+	}).catch((err)=>{
+		res.status(400).send({error:"Cannot get pokemon: Pokemon not found"})
 		return
-	}	
-	let p = pokemon.get(req.params.id-1)
-	p.type2 = req.body.type2
-	pokemon.update(p)
-	res.status(200).send(p)
+	})
+	pokemon.get(req.params.id).then((result)=>{
+		if(req.body.name != undefined)
+			result.name = req.body.name
+		if(req.body.type != undefined)
+			result.type = req.body.type
+		if(req.body.type2 != undefined)
+			result.type2 = req.body.type2
+		pokemon.update(req.params.id,result).then((result)=>{
+			res.status(200).send(result)
+		}).catch((err)=>{
+			console.log(err)
+			res.status(500).send({error:"Get Pokemon Unsuccessful"})
+		})
+	}).catch((err)=>{
+		res.status(500).send({error:"Get Pokemon Unsuccessful"})
+	})
+	
 })
 
-router.delete('/pokemon/:id', function (req, res) {
+router.delete('/pokemon/:id', (req, res) => {
 	if(checkParem(req.params.id)){
 		res.status(400).send({error:"Insufficient parameters: id is required"})		
 		return
